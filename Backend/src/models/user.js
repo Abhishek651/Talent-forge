b695775654f394192c1c335665aa69d6c0de66cc
Abhishek <bhagatabhishek772@gmail.com>
2026-07-11 15:50:33 +0530

    user schema added verified field

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -1,19 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, 
-                required: true, 
-                unique: [true, "Username already Taken"] 
-            },
-    email: { type: String,
-            unique:[ true, "Account already exist"],
-            required: true },
+  username: {
+    type: String,
+    required: true,
+    unique: [true, "Username already Taken"],
+  },
+  email: {
+    type: String,
+    unique: [true, "Account already exist"],
+    required: true,
+  },
 
-    password: { type: String, 
-                required: true },
+  password: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  otp: { type: Number, default: null },
+  otpExpiry: Number
 });
 
 //creating user named collection in database using the userSchema
-const userModal = mongoose.model('Users', userSchema);
+const userModal = mongoose.model("Users", userSchema);
 
 module.exports = userModal;
